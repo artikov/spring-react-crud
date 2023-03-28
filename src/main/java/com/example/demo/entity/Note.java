f34e8bcbf9cbbb3bfe29711693fd6394a5ddbe0d
artikov <oybekartikov@icloud.com>
2023-03-28 18:02:13 +0500

    initial commit

---
+++ src/main/java/com/example/demo/entity/Note.java
@@ -0,0 +1,22 @@
+package com.example.demo.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Data;
+
+@Entity
+@Table(name="tbl_notes")
+@Data
+public class Note {
+	
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	private Long id;
+	private String title;
+	private String body;
+	private String category;
+	
+}
