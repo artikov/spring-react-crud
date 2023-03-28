6b8cbd46b2df2b1b7fb46e6db9969257ebab8234
artikov <oybekartikov@icloud.com>
2023-03-28 20:39:15 +0500

    Complete REST

--- src/main/java/com/example/demo/controller/NotesController.java
+++ src/main/java/com/example/demo/controller/NotesController.java
@@ -1,9 +1,16 @@
 package com.example.demo.controller;
 
+
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.example.demo.entity.Note;
 import com.example.demo.repository.NotesRepository;
 
 @RestController
@@ -12,5 +19,10 @@ public class NotesController {
 	
 	@Autowired
 	NotesRepository nRepo;
+	
+	@GetMapping("/notes")
+	public ResponseEntity<List<Note>> readNotes () {
+		return new ResponseEntity<List<Note>>(nRepo.findAll(), HttpStatus.OK);
+	}
 
 }
