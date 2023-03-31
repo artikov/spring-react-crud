f42865a6203d60df655399ad24fff41100684a89
artikov <oybekartikov@icloud.com>
2023-04-01 00:05:21 +0500

    Style Navbar

--- react-notes-frontend/src/App.js
+++ react-notes-frontend/src/App.js
@@ -1,11 +1,22 @@
 import "./App.css";
 import NotesList from "./components/NotesList";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import NotFound from "./components/NotFound";
+import Navbar from "./components/Navbar";
 
 function App() {
   return (
-    <div>
-      <NotesList />
-    </div>
+    <BrowserRouter>
+      <div>
+        <Navbar />
+        <div>
+          <Routes>
+            <Route path="/" exact element={<NotesList />} />
+            <Route path="*" element={<NotFound />} />
+          </Routes>
+        </div>
+      </div>
+    </BrowserRouter>
   );
 }
 
---
+++ react-notes-frontend/src/components/NotFound.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const NotFound = () => {
+  return (
+    <div>
+      <h1>The page is not yet developed.</h1>
+    </div>
+  );
+};
+
+export default NotFound;
