3c5132a740c4c320327c955bf3dc4a8edce2472d
artikov <oybekartikov@icloud.com>
2023-04-05 18:23:32 +0500

    main functionality done, ready for prod

--- react-notes-frontend/src/components/NotesList.jsx
+++ react-notes-frontend/src/components/NotesList.jsx
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from "react";
 import NotesService from "../services/NotesService";
+import { Link } from "react-router-dom";
 
 const NotesList = () => {
   const [notes, setNotes] = useState([]);
@@ -19,15 +20,22 @@ const NotesList = () => {
   }, []);
 
   return (
-    <div>
-      <h1>List of Notes</h1>
-      {notes &&
-        notes.map((note) => (
-          <div key={note.id}>
-            <h2>{note.title}</h2>
-            <p>{note.body}</p>
-          </div>
-        ))}
+    <div className="main-content">
+      <h4>List of Notes</h4>
+      <div className="notes-list mt-4">
+        {notes.length > 0 ? (
+          notes.map((note) => (
+            <div key={note.id} className="notes-preview mt-3">
+              <Link to="#">
+                <h5 className="primary-color text-capitalize">{note.title}</h5>
+                <p>{note.body}</p>
+              </Link>
+            </div>
+          ))
+        ) : (
+          <p>No notes found</p>
+        )}
+      </div>
     </div>
   );
 };
